51524db22a51e65d9dec2f6371af5d5fd95194eb
ntyrrell <nicholas.tyrrell@monash.edu>
2015-07-13 21:22:47 +1000

    cleaned up playsong

--- playsong.py
+++ playsong.py
@@ -9,29 +9,13 @@ import os
 import sys
 
 
-# def writePLS(playlist):
-    
-
 if len(sys.argv) != 2:
 	sys.exit('Usage: %s <name of playlist>' % os.path.basename(sys.argv[0]))
 
 lcd = Adafruit_CharLCD()
+lcd.begin(16, 1)
 
 in_plist = '/home/pi/playlists/%s.txt' %sys.argv[1]
-
-lcd.begin(16, 2)
-
-def readplaylist(filestr):
-	with open(filestr) as f:
-		outstr = f.read().splitlines()
-# 	textarray = np.array([[str(n) for n in line.split()] for line in outputstr])
-	return outstr #textarray
-
-def run_cmd(cmd):
-    p = Popen(cmd, shell=True, stdout=PIPE)
-    output = p.communicate()[0]
-    return output
-
 
 cmd = 'mplayer -identify -quiet -playlist %s' %in_plist
 p = Popen(cmd, shell=True, stderr=PIPE, stdout=PIPE)
@@ -49,8 +33,8 @@ while True:
         print("Song info: %s"%song)
         print "LAUNCH MEDIATHINGY"
         info_cmd = ["mediainfo",song]
-        minfo = Popen(info_cmd, stderr=PIPE, stdout=PIPE)
-        for line in minfo.stdout.readlines():
+        medinfo = Popen(info_cmd, stdout=PIPE)
+        for line in medinfo.stdout.readlines():
             if line.startswith('Performer'):
                 artist = line.split(':', 1)[1].strip()
             if line.startswith('Track name '):
@@ -78,20 +62,3 @@ while True:
 # 		sys.stdout.write(out)
         sys.stdout.flush()
 
-# songname = run_cmd(cmd)
-# lcd.message(songname)
-# lcd.message('IP %s' % (ipaddr))
-
-#p= subprocess.Popen(['mplayer', url], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#for line in p.stdout:
-#    if line.startswith('ICY Info:'):
-#        info = line.split(':', 1)[1].strip()
-#        attrs = dict(re.findall("(\w+)='([^']*)'", info))
-#        print 'Stream title: '+attrs.get('StreamTitle', '(none)')
-#    sleep(2)
-#while 1:
-#    lcd.clear()
-#    ipaddr = run_cmd(cmd)
-#    lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
-#    lcd.message('IP %s' % (ipaddr))
-#    sleep(2)
